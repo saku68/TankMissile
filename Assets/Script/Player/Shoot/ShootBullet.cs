df9b612a457ba7e6dfc5f063ded89e5bc36800c5
Saku68 <tekeheshinsaku@gmail.com>
2024-05-10 16:51:25 +0900

    水平二連と三方向発射を切り替え可能にしてみたところ三方向の中央部が左右に引かれてしまう挙動を確認

--- Assets/Script/Player/Shoot/ShootBullet.cs
+++ Assets/Script/Player/Shoot/ShootBullet.cs
@@ -8,13 +8,12 @@ using System.Diagnostics.CodeAnalysis;
 
 public class ShootBullet : MonoBehaviour
 {
+    public int shootMode = 1;
     public float fireRate = 1f; // 発射速度（1秒あたりの発射回数）
 
     private float nextFireTime = 0f; // 次に発射できる時刻
-    [SerializeField]
-    private float offset = 0.5f; // 発射位置の間隔
-    [SerializeField]
-    private float angle = 15f; // 発射角度
+    public float offset = 0.5f; // 発射位置の間隔
+    public float angle = 15f; // 発射角度
 
     /// 弾のPrefab
     [SerializeField, Tooltip("弾のPrefab")]
@@ -54,60 +53,79 @@ public class ShootBullet : MonoBehaviour
     {
         if (Time.time >= nextFireTime)
         {
-            // 弾を生成して飛ばす
-            // GameObject obj = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
-            // Rigidbody rid = obj.GetComponent<Rigidbody>();
-            // rid.AddForce(shootVelocity * rid.mass, ForceMode.Impulse);
-            // // 次の発射可能時刻を更新する
-            // nextFireTime = Time.time + 1f / fireRate;
-            // // 5秒後に消える
-            // Destroy(obj, 5.0F);
-
-            // //水平二連発射
-            // // 弾を生成して飛ばす
-            // Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
-            // // Vector3 positionOffsetUp = barrelObject.transform.up * offset; // バレルの上方向へのオフセット
-            // GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
-            // GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
-
-            // Rigidbody rid1 = obj1.GetComponent<Rigidbody>();
-            // Rigidbody rid2 = obj2.GetComponent<Rigidbody>();
-
-            // rid1.AddForce(shootVelocity * rid1.mass, ForceMode.Impulse);
-            // rid2.AddForce(shootVelocity * rid2.mass, ForceMode.Impulse);
-
-            // // 次の発射可能時刻を更新する
-            // nextFireTime = Time.time + 1f / fireRate;
-            // // 5秒後に消える
-            // Destroy(obj1, 5.0F);
-            // Destroy(obj2, 5.0F);
-
-            // //三方向発射
-            // // バレルの向きに基づいて角度を回転させる
-            // Quaternion rotationRight = Quaternion.AngleAxis(angle, instantiatePosition);
-            // Quaternion rotationLeft = Quaternion.AngleAxis(-angle, instantiatePosition);
-
-            // // 弾を生成して飛ばす
-            // Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
-            // // Vector3 positionOffsetUp = barrelObject.transform.up * offset; // バレルの上方向へのオフセット
-            // GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
-            // GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
-            // GameObject obj3 = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
-
-            // Rigidbody rid1 = obj1.GetComponent<Rigidbody>();
-            // Rigidbody rid2 = obj2.GetComponent<Rigidbody>();
-            // Rigidbody rid3 = obj3.GetComponent<Rigidbody>();
-
-            // rid1.AddForce(rotationRight * shootVelocity * rid1.mass, ForceMode.Impulse);
-            // rid2.AddForce(rotationLeft * shootVelocity * rid2.mass, ForceMode.Impulse);
-            // rid3.AddForce(shootVelocity * rid3.mass, ForceMode.Impulse);
-
-            // // 次の発射可能時刻を更新する
-            // nextFireTime = Time.time + 1f / fireRate;
-            // // 5秒後に消える
-            // Destroy(obj1, 5.0F);
-            // Destroy(obj2, 5.0F);
-            // Destroy(obj3, 5.0F);
+            switch (shootMode)
+            {
+                case 1:
+                    ShootSingle();
+                    break;
+                case 2:
+                    ShootHorizontal();
+                    break;
+                case 3:
+                    ShootTriple();
+                    break;
+            }
         }
+    }
+    private void ShootSingle()
+    {
+        // 弾を生成して飛ばす
+        GameObject obj = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
+        Rigidbody rid = obj.GetComponent<Rigidbody>();
+        rid.AddForce(shootVelocity * rid.mass, ForceMode.Impulse);
+        // 次の発射可能時刻を更新する
+        nextFireTime = Time.time + 1f / fireRate;
+        // 5秒後に消える
+        Destroy(obj, 5.0F);
+    }
+
+    private void ShootHorizontal()
+    {
+        Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
+
+        // 弾を生成して飛ばす
+        GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
+        GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
+
+        Rigidbody rid1 = obj1.GetComponent<Rigidbody>();
+        Rigidbody rid2 = obj2.GetComponent<Rigidbody>();
+
+        rid1.AddForce(shootVelocity * rid1.mass, ForceMode.Impulse);
+        rid2.AddForce(shootVelocity * rid2.mass, ForceMode.Impulse);
+
+        // 次の発射可能時刻を更新する
+        nextFireTime = Time.time + 1f / fireRate;
+        // 5秒後に消える
+        Destroy(obj1, 5.0F);
+        Destroy(obj2, 5.0F);
+    }
+
+    private void ShootTriple()
+    {
+        Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
+
+        // バレルの向きに基づいて角度を回転させる
+        Quaternion rotationRight = Quaternion.AngleAxis(angle, instantiatePosition);
+        Quaternion rotationLeft = Quaternion.AngleAxis(-angle, instantiatePosition);
+
+        // 弾を生成して飛ばす
+        GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
+        GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
+        GameObject obj3 = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
+
+        Rigidbody rid1 = obj1.GetComponent<Rigidbody>();
+        Rigidbody rid2 = obj2.GetComponent<Rigidbody>();
+        Rigidbody rid3 = obj3.GetComponent<Rigidbody>();
+
+        rid1.AddForce(rotationRight * shootVelocity * rid1.mass, ForceMode.Impulse);
+        rid2.AddForce(rotationLeft * shootVelocity * rid2.mass, ForceMode.Impulse);
+        rid3.AddForce(shootVelocity * rid3.mass, ForceMode.Impulse);
+
+        // 次の発射可能時刻を更新する
+        nextFireTime = Time.time + 1f / fireRate;
+        // 5秒後に消える
+        Destroy(obj1, 5.0F);
+        Destroy(obj2, 5.0F);
+        Destroy(obj3, 5.0F);
     }
 }
