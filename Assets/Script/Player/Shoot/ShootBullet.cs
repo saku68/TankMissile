8f8d7e7467a32b3d100d21b6dd06ae814d2edfb7
Saku68 <tekeheshinsaku@gmail.com>
2024-06-06 06:58:06 +0900

    砲台の上下制限を調整し、コードを整理。制限のチェックと角度の正規化を別の関数として分けた。

--- Assets/Script/Player/Shoot/ShootBullet.cs
+++ Assets/Script/Player/Shoot/ShootBullet.cs
@@ -15,46 +15,41 @@ public class ShootBullet : MonoBehaviour
     public float angle = 15f; // 同時発射角度
     private float nextFireTime = 0f; // 次に発射できる時刻
 
-    /// 弾のPrefab
     [SerializeField, Tooltip("弾のPrefab")]
     private GameObject bulletPrefab;
 
-    /// 砲身のオブジェクト
     [SerializeField, Tooltip("砲身のオブジェクト")]
     private GameObject barrelObject;
 
-    /// 弾を生成する位置情報
     private Vector3 instantiatePosition;
 
-    /// 弾の生成座標(読み取り専用)
     public Vector3 InstantiatePosition => instantiatePosition;
 
-    /// 弾の速さ
     [Range(3.0F, 100.0F), Tooltip("弾の射出する速さ")]
     public float bulletSpeed = 9.0F;
 
-    /// 弾の初速度
     private Vector3 shootVelocity;
 
-    /// 弾の初速度(読み取り専用)
     public Vector3 ShootVelocity
     {
         get { return shootVelocity; }
     }
+
     void Start()
     {
         Dameger dameger = bulletPrefab.GetComponent<Dameger>();
         dameger.damage2 = 1;
         bulletPrefab.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
     }
+
     void Update()
     {
-        //UniRxでUpdateから出せないか
         // 弾の初速度を更新
-        shootVelocity = barrelObject.transform.up * bulletSpeed;
+        shootVelocity = barrelObject.transform.forward * bulletSpeed;
         // 弾の生成座標を更新
         instantiatePosition = barrelObject.transform.position;
     }
+
     public void Shoot()
     {
         if (Time.time >= nextFireTime)
@@ -73,23 +68,20 @@ public class ShootBullet : MonoBehaviour
             }
         }
     }
+
     private void ShootSingle()
     {
-        // 弾を生成して飛ばす
         GameObject obj = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
         Rigidbody rid = obj.GetComponent<Rigidbody>();
         rid.AddForce(shootVelocity * rid.mass, ForceMode.Impulse);
-        // 次の発射可能時刻を更新する
         nextFireTime = Time.time + 1f / fireRate;
-        // 5秒後に消える
         Destroy(obj, 5.0F);
     }
 
     private void ShootHorizontal()
     {
-        Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
+        Vector3 positionOffsetRight = barrelObject.transform.right * offset;
 
-        // 弾を生成して飛ばす
         GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
         GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
 
@@ -99,22 +91,18 @@ public class ShootBullet : MonoBehaviour
         rid1.AddForce(shootVelocity * rid1.mass, ForceMode.Impulse);
         rid2.AddForce(shootVelocity * rid2.mass, ForceMode.Impulse);
 
-        // 次の発射可能時刻を更新する
         nextFireTime = Time.time + 1f / fireRate;
-        // 5秒後に消える
         Destroy(obj1, 5.0F);
         Destroy(obj2, 5.0F);
     }
 
     private void ShootTriple()
     {
-        Vector3 positionOffsetRight = barrelObject.transform.right * offset; // バレルの右方向へのオフセット
+        Vector3 positionOffsetRight = barrelObject.transform.right * offset;
 
-        // バレルの向きに基づいて角度を回転させる
-        Quaternion rotationRight = Quaternion.AngleAxis(angle, instantiatePosition);
-        Quaternion rotationLeft = Quaternion.AngleAxis(-angle, instantiatePosition);
+        Quaternion rotationRight = Quaternion.AngleAxis(angle, barrelObject.transform.up);
+        Quaternion rotationLeft = Quaternion.AngleAxis(-angle, barrelObject.transform.up);
 
-        // 弾を生成して飛ばす
         GameObject obj1 = Instantiate(bulletPrefab, instantiatePosition + positionOffsetRight, Quaternion.identity);
         GameObject obj2 = Instantiate(bulletPrefab, instantiatePosition - positionOffsetRight, Quaternion.identity);
         GameObject obj3 = Instantiate(bulletPrefab, instantiatePosition, Quaternion.identity);
@@ -127,9 +115,7 @@ public class ShootBullet : MonoBehaviour
         rid2.AddForce(rotationLeft * shootVelocity * rid2.mass, ForceMode.Impulse);
         rid3.AddForce(shootVelocity * rid3.mass, ForceMode.Impulse);
 
-        // 次の発射可能時刻を更新する
         nextFireTime = Time.time + 1f / fireRate;
-        // 5秒後に消える
         Destroy(obj1, 5.0F);
         Destroy(obj2, 5.0F);
         Destroy(obj3, 5.0F);
@@ -140,6 +126,7 @@ public class ShootBullet : MonoBehaviour
         Dameger dameger = bulletPrefab.GetComponent<Dameger>();
         dameger.damage2 += newDamage;
     }
+
     public void ChangeBulletSize(Vector3 newSize)
     {
         bulletPrefab.transform.localScale = newSize;
