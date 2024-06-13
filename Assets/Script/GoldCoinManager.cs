d4e1d282c949e9634257eefa4233ae551c94e497
Saku68 <tekeheshinsaku@gmail.com>
2024-06-14 06:09:29 +0900

    死亡時コインがプレイヤーを見失うエラーはNullチェックをしてスキップ処理を行い解決。

--- Assets/Script/GoldCoinManager.cs
+++ Assets/Script/GoldCoinManager.cs
@@ -21,24 +21,23 @@ public class GoldCoinManager : MonoBehaviour
 
     void Update()
     {
-        if (!playerTransform == null)
-        {
-            // プレイヤーの方向を向く
-            Vector3 direction = (playerTransform.position - transform.position).normalized;
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        // プレイヤーが存在しない場合は処理をスキップ
+        if (playerTransform == null) return;
+        // プレイヤーの方向を向く
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
 
-            // Raycastでプレイヤーとの障害物の有無を確認
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, direction, out hit, followDistance))
+        // Raycastでプレイヤーとの障害物の有無を確認
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, followDistance))
+        {
+            if (hit.collider.CompareTag("Player"))
             {
-                if (hit.collider.CompareTag("Player"))
-                {
-                    // プレイヤーに向かって移動
-                    // rb.velocity = direction * 5f; // 移動速度は適宜調整してください
-                    // 上方向の力を加えて打ち出す
-                    rb.velocity = direction * launchForce + Vector3.up * upwardForce;
-                }
+                // プレイヤーに向かって移動
+                // rb.velocity = direction * 5f; // 移動速度は適宜調整してください
+                // 上方向の力を加えて打ち出す
+                rb.velocity = direction * launchForce + Vector3.up * upwardForce;
             }
         }
     }
